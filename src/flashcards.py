b65335725f7f2727fc13ba067d65f8dc4b6da6b0
Piotr Bajsarowicz <piotr.bajsarowicz@gmail.com>
2024-01-05 00:50:40 +0100

    [FC-4] Adjust layout

--- src/flashcards.py
+++ src/flashcards.py
@@ -6,5 +6,5 @@ from google_sheets import google_sheets
 class Flashcards:
     FLASHCARDS_SPREADSHEET_ID = os.environ["FLASHCARDS_SPREADSHEET_ID"]
 
-    def pull(self) -> list:
+    def pull(self) -> list[list[str]]:
         return google_sheets.get_data(self.FLASHCARDS_SPREADSHEET_ID)
--- src/main.py
+++ src/main.py
@@ -1,16 +1,44 @@
-import json
-
 from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.label import Label
 
 from flashcards import Flashcards
 
 
+class FlashcardsLayout(FloatLayout):
+    HEADS = 0
+    TAILS = 1
+    DEFAULT_TEXT = "NO MORE FLASHCARDS"
+
+    def __init__(self, **kwargs) -> None:
+        self.flashcards = kwargs.pop("flashcards")
+        super().__init__(**kwargs)
+        self.current_flashcard_idx = 0
+        self.build()
+
+    def _set_flashcard(self, state: int) -> None:
+        self.ids.flashcard.text = self.flashcards[self.current_flashcard_idx][state]
+
+    def next_flashcard(self) -> None:
+        self.current_flashcard_idx += 1
+        try:
+            self._set_flashcard(self.HEADS)
+        except IndexError:
+            self.current_flashcard_idx = 0
+            self._set_flashcard(self.HEADS)
+
+    def reveal_flashcard(self) -> None:
+        self._set_flashcard(self.TAILS)
+
+    def build(self) -> None:
+        self._set_flashcard(self.HEADS)
+
+
 class FlashcardsApp(App):
     def build(self) -> Label:
         flashcards = Flashcards()
-        data = flashcards.pull()
-        return Label(text=json.dumps(data))
+        flashcards_data = flashcards.pull()
+        return FlashcardsLayout(flashcards=flashcards_data)
 
 
 if __name__ == "__main__":
